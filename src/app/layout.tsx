6622b381dcea79d02978d253d764faea08d14411
Galih Prastian <galih@wartek.belajar.id>
2023-09-19 04:24:57 +0700

    feat: create global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Nav } from '@/components/nav'
+import { Footer } from '@/components/footer'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -10,13 +12,21 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={'h-full'}>
+    <body className={inter.className + ' h-full'}>
+    <div className="max-w-2xl mx-auto py-10 px-4">
+      <Nav/>
+      <main>
+        {children}
+      </main>
+      <Footer/>
+    </div>
+    </body>
     </html>
   )
 }
---
+++ src/components/nav.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+export function Nav() {
+  return (
+    <nav className="mb-12">
+      <div className="container mx-auto">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center mr-auto">
+              <Link href="/" className="text-3xl font-black">
+                BuyABlog
+              </Link>
+            </div>
+            <div className="hidden sm:ml-12 sm:flex sm:items-center">
+              <div className="space-x-6 text-sm font-medium">
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
