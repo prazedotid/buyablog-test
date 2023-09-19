39a3e7c328e82a48a035ecc09a924f0a70c60e47
Galih Prastian <galih@wartek.belajar.id>
2023-09-20 03:35:28 +0700

    feat: init next-auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,37 @@
+import { compare } from 'bcrypt'
+import { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+import prisma from '@/lib/prisma'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'admin@buyablog.com' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials, req) {
+        const { username, password } = credentials as {
+          username: string
+          password: string
+        }
+
+        const user = await prisma.users.findUnique({
+          where: { username },
+        })
+        if (!user) {
+          return null
+        }
+
+        const isValidPassword = await compare(password, user.password)
+        if (!isValidPassword) {
+          throw new Error('Invalid password.')
+        }
+
+        return user
+      }
+    })
+  ],
+}
