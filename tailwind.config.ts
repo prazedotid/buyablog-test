171d9f979a48b630c89116dab32710fc46c58df7
Galih Prastian <galih@wartek.belajar.id>
2023-09-22 02:26:17 +0700

    fix: fix datepicker not styled properly

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/tailwind-datepicker-react/dist/**/*.js',
+    './node_modules/@prazedotid/tailwind-datepicker-react/dist/**/*.js',
   ],
   theme: {
     extend: {
